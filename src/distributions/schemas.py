7fa239767299c2ea333d65b757456fc754f1720a
0x3EFFFC8 <x3r0_sexy@outlook.com>
2026-08-05 15:30:13 -0500

    feat: add Pydantic schemas for Distribution

---
+++ src/distributions/schemas.py
@@ -0,0 +1,18 @@
+import uuid 
+from datetime import datetime
+from typing import List, Optional
+from pydantic import BaseModel 
+
+
+class DistributionModel(BaseModel):
+    uid: uuid.UUID
+    name: str
+    alternatenames: str
+    created_at: datetime
+    updated_at: datetime
+    dragons_uids: Optional[List[uuid.UUID]] = []
+
+
+class DistributionCreateModel(BaseModel):
+    name: str 
+    alternatenames: str 
